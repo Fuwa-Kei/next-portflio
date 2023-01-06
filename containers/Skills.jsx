4b3fbf6c9a1d07522a06f10567c3f749898bffa1
Kei Chan 'Fuwa <116655254+Fuwa-Kei@users.noreply.github.com>
2023-01-06 03:48:11 +0000

    adjusted grid positioning

--- containers/Skills.jsx
+++ containers/Skills.jsx
@@ -7,7 +7,7 @@ function Skills() {
     <div className="w-full px-8">
         <div className="max-w-[1024px] m-auto flex flex-col justify-center items-center gap-4">
             <h3 className="uppercase">Skills | Tech stack</h3>
-            <div className="my-8 grid md:grid-cols-2 lg:grid-cols-4 gap-8">
+            <div className="grid md:grid-cols-2 lg:grid-cols-4 gap-8">
                 <SkillCard img={html5} text={"HTML5"} />
                 <SkillCard img={css} text={"CSS"} />
                 <SkillCard img={js} text={"Javascript"} />
--- containers/components/SkillCard.jsx
+++ containers/components/SkillCard.jsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 function SkillCard (props) {
   return (
-    <div className="flex-1 flex justify-center gap-8 items-center">
+    <div className="flex-1 flex justify-start gap-8 items-center">
         <Image src={props.img} width={100} height={100} className="hover:scale-105 ease-in duration-300" />
         <p>{props.text}</p>
     </div>
