f4f70d94330821c135af76a7e43d60f0cf377e05
Kei Chan 'Fuwa <116655254+Fuwa-Kei@users.noreply.github.com>
2023-02-05 13:49:36 +0000

    Update study.jsx

--- pages/study.jsx
+++ pages/study.jsx
@@ -15,9 +15,9 @@ function study() {
           <h4>Hackathon project for MLH - Eduhacks</h4>
           <p>A co-study platform where users can register and join virtual rooms to study together with other people</p>
           <br />
-          <p>For demo purposes, click login for profile and the first room to access the chatroom</p>
+          <p>For demo purposes, click LOGIN t access the profile and the first room to access the chatroom.</p>
           
-          <p>Login email: demo@demo.com | password: Demo123</p>
+          <p>Login email: demo@demo.com | Password: Demo123</p>
           <p className="text-[#FF8A5B] flex gap-8 justify-center mt-4">
             <Link href='https://github.com/Fuwa-Kei/studyperks--front-end-only-' target="_blank">Github</Link>
            | 
@@ -30,11 +30,11 @@ function study() {
             </div>
             <div className="col-span-1 pt-8 md:pt-0 text-justify">
                 <h4 className=" text-[#EA526F]">My Role </h4>
-                <p>I was responsible for designing and implementing the front-end of the application
-                The wire framing and low fidelity prototype were done in Figma. After I created a high fidelity prototype following the design using HTML and CSS.
-                Finally I used react to create the front-end of the web application</p>
+                <p>I was responsible for designing and implementing the front-end of the application.
+              The wire framing and low fidelity prototype were completed in Figma. I then created a high fidelity prototype following the design using HTML and CSS.
+              Finally I used React to create the front-end of the web application.</p>
                 <h4 className="  text-[#EA526F] mt-4">What I Learnt</h4>
-                <p>When creating my first project I noticed I was repeating myself a lot especially when it came to the css, so this time I made sure my my codes DRY</p>
+                <p>When creating my first project I noticed I was repeating myself a lot especially when it came to the CSS, so this time I made sure my codes are DRY.</p>
                 <p>Main skills learnt from this project are:</p>
                   <ul className="list-disc pl-11">
                     <li>Creating popup/modals</li>
@@ -46,9 +46,9 @@ function study() {
           <div className=" max-w-[1024px] m-auto md:grid grid-cols-2 gap-8 items-center px-8 my-20">
             <div className="col-span-1 pt-8 md:pt-0 text-justify">
               <h4 className=" text-[#EA526F]">Difficulties I had</h4>
-              <p>I ran into an issue were no matter what I did I wasnt able to get a 2nd modal to appear but luckily managed to solve the problem with SweetAlert. Firebase also gave me many error messages to solve and is still something I need to study a lot more on </p>
-              <h4 className="  text-[#EA526F] mt-4">Whats next for StudyPerks</h4>
-              <p>This was a project I really enjoyed creating and plan to complete it with fully functioning chatrooms including a pomodoro timer and games. I would also love to be able update the profile so that it will display actual stats for each user. </p>
+              <p>I ran into an issue where no matter what I did I wasn't able to get a 2nd modal to appear but luckily managed to solve the problem with SweetAlert. Firebase also gave me many error messages to solve and is still something I need to study a lot more on. </p>
+              <h4 className="  text-[#EA526F] mt-4">What's next for StudyPerks</h4>
+              <p>This was a project I really enjoyed creating and plan to complete it with fully functioning chatrooms including a pomodoro timer and games. I would also love to be able to update the profile so that it will display actual stats for each user. </p>
             </div>
             <div>
               <Image src={profile} alt="profile image" width={1000} className="width-300 mx-auto border border-white-500 rounded-lg md:hover:scale-150 ease-in duration-300" />
