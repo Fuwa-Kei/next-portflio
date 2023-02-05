e8bf998f311aea4f347a2d68a52e97c2960a1162
Kei Chan 'Fuwa <116655254+Fuwa-Kei@users.noreply.github.com>
2023-02-05 14:01:14 +0000

    Update others.jsx

--- pages/others.jsx
+++ pages/others.jsx
@@ -20,8 +20,8 @@ function others() {
             </div>
             <div className="col-span-1 pt-8 md:pt-0 text-justify">
                 <h4 className=" text-[#2EC4B6]">My CV & Portfolio  </h4>
-                <p>Nextjs | Tailwind</p>
-                <p className="mt-4">I wanted to leant about nextjs and tailwind and decided to try them out by building my cv due to the simple nature of what a cv is. This allowed me to understand how tailwind works before I moved onto a bigger project, my portfolio</p>
+                <p>Next.js | Tailwind</p>
+                <p className="mt-4">I wanted to learn about Next.js and Tailwind and decided to try them out by building my CV due to the simple nature of what a CV is. This allowed me to understand how Tailwind works before I moved onto a bigger project - my portfolio.</p>
                 <p className="mt-4">Github: <Link href='https://github.com/Fuwa-Kei/cv-tailwind' target="_blank">CV</Link> | <Link href='https://github.com/Fuwa-Kei/next-portflio' target="_blank">Portfolio</Link></p>
               </div>
           </div>
@@ -30,7 +30,7 @@ function others() {
             <div className="col-span-1 pt-8 md:pt-0 text-justify">
             <h4 className=" text-[#2EC4B6]">Pomodoro Timer </h4>
                 <p>Javascript</p>
-                <p className="mt-4">Made this while working on the Studyperks projects to be used in the study rooms. I plan on improving this timer by allowing users to set thir own pomodoro times and also intergratign it with my studyperks project </p>
+                <p className="mt-4">I made this while working on the Studyperks project to be used in the study rooms. I plan on improving this timer by allowing users to set their own pomodoro times and also integrating it with my Studyperks project. </p>
                 <p className="mt-4"><Link href='https://github.com/Fuwa-Kei/Pomodoro-Timer' target="_blank">Github</Link> | <Link href='https://fuwa-kei.github.io/Pomodoro-Timer/' target="_blank">Demo</Link></p>
               </div>
             <div>
@@ -44,8 +44,8 @@ function others() {
             </div>
             <div className="col-span-1 pt-8 md:pt-0 text-justify">
                 <h4 className=" text-[#2EC4B6]">Pokedex</h4>
-                <p>React | pokeapi</p>
-                <p className="mt-4">Decided to create a pokedex like web application to learn about how to get data from APIs. I styled the page imitate the pokedex entries in the original Pokemon games released on the Gameboy Color</p>
+                <p>React | Pokeapi</p>
+                <p className="mt-4"> I decided to create a Pokedex like web application to learn about how to get data from APIs. I styled the page to imitate the Pokedex entries in the original Pokemon games released on the Gameboy Color.</p>
                 <p className="mt-4"><Link href='https://github.com/Fuwa-Kei/pokedex' target="_blank">Github</Link> | <Link href='https://pokedex-fuwa-kei.vercel.app/' target="_blank">Demo</Link></p>
               </div>
           </div>
@@ -54,7 +54,7 @@ function others() {
             <div className="col-span-1 pt-8 md:pt-0 text-justify">
             <h4 className=" text-[#2EC4B6]">Simon&apos;s Game</h4>
                 <p>JQuery</p>
-                <p className="mt-4">This was created as part of the Angelu Yu&apos;s web development bootcamp course. This was one of the first &apos;complete&apos; projects I made and it really helped me understand the basics of how javascript(jquery) worked</p>
+                <p className="mt-4">This was created as part of Angelu Yu&apos;s web development bootcamp course. This was one of the first &apos;complete&apos; projects I made and it really helped me understand the basics of how Javascript(Jquery) worked.</p>
                 <p className="mt-4"><Link href='https://github.com/Fuwa-Kei/Simon-Game' target="_blank">Github</Link> | <Link href='https://fuwa-kei.github.io/Simon-Game/' target="_blank">Demo</Link></p>
               </div>
             <div>
